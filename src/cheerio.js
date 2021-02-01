f43d915a429036f2352e30e93405678b02968577
zygis <zygis.1415@outlook.es>
2021-02-01 12:27:45 +0100

    feat:added basic structure and cheerio example

---
+++ src/cheerio.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+
+async function scrapingTime() {
+  const response = await fetch("https://www.amazon.es/gp/bestsellers/books");
+  const text = await response.text();
+  const $ = cheerio.load(text);
+
+  $("ol > li > span > div > span").each(function (i, elm) {
+    const bookTitle = $(elm).find("a div").text().trim();
+    const bookAuthor = $(elm).find("> div:nth-of-type(1) span").text().trim(); // Empieza en 1 y no 0
+    const bookImg = $(elm).find("> a > span > div > img").attr("src");
+    const bookHref = $(elm).find("> a").attr("href");
+    const bookLink = `https://www.amazon.es${bookHref}`;
+    const bookScore = $(elm).find("> div:nth-of-type(2) span").text();
+    const bookPrice = $(elm)
+      .find("> div:nth-of-type(4) a > span > span")
+      .text();
+
+    console.log({
+      top: i + 1,
+      bookTitle,
+      bookAuthor,
+      bookImg,
+      bookLink,
+      bookScore,
+      bookPrice,
+    });
+  });
+}
+
+scrapingTime();
